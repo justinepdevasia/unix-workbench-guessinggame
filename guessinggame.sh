234bc62788a4c3bb0f7f9d59f8b9f982d1551f8c
justinepdevasia <justinepdevasia@gmail.com>
2020-05-29 13:39:43 +0530

    added modifictions include corner cases and counting hidden files

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,18 +3,27 @@ function congratulate {
 	echo "Congratulations!!! your guess is correct"
 }
 
-actual=$(ls | wc -l )
+actual=$(ls -a | wc -l )
 read -p "Guess the number of files in the directory " guess
+
+num='^[0-9]+$'
+if ! [[ $guess =~ $num ]] 
+then
+   echo "Sorry wrong input : Not a number" 
+   exit 1
+fi
+
 while [ $guess -ne $actual ]
 do
 	if [[ $guess -lt $actual ]]
-		then
+	then
 			echo "guess is less"
 	else
 		echo "guess is more"
 	fi
 	read -p "Guess the number of files in the directory " guess
 done
+
 if [[ $guess -eq $actual ]]
 then
 	congratulate
